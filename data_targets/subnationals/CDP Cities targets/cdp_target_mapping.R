6f32c5928d3f58ea6e8e778caca2e845d43047c9
yeozy95 <zhiyi.yeo@u.yale-nus.edu.sg>
2022-11-03 12:33:06 -0400

    Update CDP Cities targets harmonized

--- data_targets/subnationals/CDP Cities targets/cdp_target_mapping.R
+++ data_targets/subnationals/CDP Cities targets/cdp_target_mapping.R
@@ -8,85 +8,130 @@ library(tidyverse)
 x <- read.csv("Raw/2021_Cities_Emissions_Reduction_Targets_clean_names.csv",
               stringsAsFactors = F, encoding = "UTF-8")
 # Map UN Locodes - use the ingested version in OC github
-locodes <- read.csv("../../../../OpenClimate-UNLOCODE/UNLOCODE/ActorName.csv",
-                    stringsAsFactors = F, fileEncoding = "UTF-8")
+# locodes <- read.csv("../../../../OpenClimate-UNLOCODE/UNLOCODE/ActorName.csv",
+#                     stringsAsFactors = F, fileEncoding = "UTF-8")
 kd_lc <- read.csv("../../../key_dict_LOCODE_to_climactor.csv",
                   stringsAsFactors = F, fileEncoding = "UTF-8")
 # Quick check
-length(unique(x$name))
-sum(unique(x$name) %in% locodes$name)
-sum(unique(x$name) %in% kd_lc$right)
-sum(unique(x$name) %in% kd_lc$right)
-sum(unique(paste0(x$name, x$iso)) %in% paste0(kd_lc$right, kd_lc$iso))
-any(is.na(kd_lc$locode))
-
-# Check if we can get more copies by using the key dict
-all_name <- key_dict[which(key_dict$right %in% x$name), ]
-length(unique(all_name$right))
-length(unique(all_name$right[(which(all_name$wrong %in% locodes$name))]))
-
-# Can get about 600+ matches by using key dict. Probably just manually clean the rest?
+# length(unique(x$name))
+# sum(unique(x$name) %in% locodes$name)
+# sum(unique(x$name) %in% kd_lc$right)
+# sum(unique(x$name) %in% kd_lc$right)
+# sum(unique(x$name) %in% kd_lc$wrong)
+# sum(unique(paste0(x$name, x$iso)) %in% paste0(kd_lc$right, kd_lc$iso))
+# any(is.na(kd_lc$locode))
 
 # Change data to OC schema first and incorporate updates from before
 
 datasource_id <- "CDPCitiesTargets:2021"
 
-ActorIdentifier <- x %>%
-  select(Account.Number) %>% 
-  rename("identifier" = Account.Number) %>%
-  mutate(namespace = "CDP")
+# Merge in Locodes
+x <- x %>% 
+  left_join(kd_lc %>% select(right, iso, locode), by = c("name" = "right", 
+                                                         "iso" = "iso"))
+# Filter for those with only unique locodes
+x <- x %>% 
+  group_by(name, iso, entity_type) %>%
+  mutate(num_locode = length(unique(locode))) %>%
+  filter(!is.na(locode) & num_locode == 1) %>%
+  filter(!is.na(Type.of.target) & Type.of.target != "No target")
 
 Actor <- x %>% 
-  select(name, entity_type, iso) %>%
-  rename("type" = entity_type)
+  select(name, entity_type, iso, locode) %>%
+  rename(
+    "actor_id" = locode,
+    "name" = name,
+    "iso" = iso,
+    "type" = entity_type,
+  ) %>%
+  distinct()
 
-Actor2 <- Actor %>% left_join(kd_lc %>% select(right, iso, locode), by = c("name" = "right", 
-                                  "iso" = "iso"))
-# Export NAs for gapfilling 
-missing_locode <- Actor2 %>% 
-  filter(is.na(locode))
-which(paste0(missing_locode$name, missing_locode$iso) %in% paste0(locodes$name, locodes$iso3))
+ActorIdentifier <- x %>%
+  ungroup() %>%
+  select(Account.Number, locode) %>% 
+  rename(
+    "actor_id" = locode,
+    "identifier" = Account.Number
+  ) %>%
+  mutate(namespace = "CDP") %>%
+  distinct()
 
-missing_locode$locode <- locodes$actor_id[match(paste0(missing_locode$name, missing_locode$iso), paste0(locodes$name, locodes$iso3))]
-# missing_locode %>% filter(is.na(locode)) %>%
-  # write.csv("missing_locodes2.csv", row.names = F, fileEncoding = "UTF-8")
-
-# Check for partial matches and focus search on those with partial searches 
-# Export for manual checks 
-# write.csv(missing_locode, "missing_locode_gapfilling.csv",
-#           row.names = F, fileEncoding = "UTF-8")
-
+Tag <- data.frame(
+  tag_id = c("BAU", "self-reported", "baseline_em", "target_em"),
+  tag_name = c("Business-as-usual scenario", "Self-reported", "Baseline emissions", "Target emissions")
+)
 
 Target <- x %>%
-  select(Type.of.target, Base.year, Percentage.reduction.target, Target.year, Sector, 
-         Target.boundary.relative.to.city.boundary,
+  ungroup() %>%
+  select(locode, Type.of.target, Base.year, Percentage.reduction.target, Target.year, Sector, 
+         Target.boundary.relative.to.city.boundary,Target.year.absolute.emissions..metric.tonnes.CO2e.,
          Intensity.unit..Emissions.per., Estimated.business.as.usual.absolute.emissions.in.target.year..metric.tonnes.CO2e.,
          Percentage.of.target.achieved.so.far, Last.update) %>%
-  rename("target_type" = Type.of.target,
-         "baseline_year" = Base.year,
-         "target_value" = Percentage.reduction.target,
-         "sector_list" = Sector,
-         "target_year" = Target.year,
-         "target_unit"= "%",
-         "target_boundary" = Target.boundary.relative.to.city.boundary,
-         "bau_value" = Estimated.business.as.usual.absolute.emissions.in.target.year..metric.tonnes.CO2e.,
-         "percent_achieved" = Percentage.of.target.achieved.so.far,
-         "last_updated" = Last.update) %>%
-  mutate(data_source = datasource_id)
+  mutate(target_type = case_when(Type.of.target == "Baseline scenario (business as usual) target" ~ "Relative emission reduction",
+                                 Type.of.target == "Base year emissions (absolute) target" ~ "Absolute emission reduction",
+                                 Type.of.target == "Base year intensity target" ~ "Carbon intensity reduction",
+                                 Type.of.target == "Fixed level target" ~ "Absolute emission reduction"),
+         baseline_year = case_when(target_type == "Relative emission reduction" ~ Target.year,
+                                   Type.of.target == "Fixed level target" ~ NA_integer_,
+                                   TRUE ~ Base.year),
+         target_id = paste0(datasource_id, ":", locode),
+         target_value = case_when(Type.of.target == "Fixed level target" ~ Target.year.absolute.emissions..metric.tonnes.CO2e.,
+                                  TRUE ~ Percentage.reduction.target),
+         target_unit= case_when(Type.of.target == "Fixed level target" ~ "tCO2e",
+                                TRUE ~ "percent"),
+         data_source = datasource_id) %>% 
+  rename(
+    "actor_id" = locode,
+    "sector_list" = Sector,
+    "target_year" = Target.year,
+    "target_boundary" = Target.boundary.relative.to.city.boundary,
+    "percent_achieved" = Percentage.of.target.achieved.so.far,
+    "last_updated" = Last.update
+  ) %>% 
+  select(-grep("\\.", names(.)))
+
+TargetTag <- Target %>%
+  mutate(self_reported = 1, 
+         BAU = case_when(target_type == "Relative emission reduction" ~ 1, 
+                         TRUE ~ 0)) %>%
+  pivot_longer(cols = c(BAU, self_reported),
+               names_to = "tag_id") %>%
+  filter(value == 1) %>%
+  select(target_id, tag_id)
 
 EmissionsAgg <- x %>%
-  select(Base.year.emissions..metric.tonnes.CO2e., Base.year, Last.update,
-         Target.year.absolute.emissions..metric.tonnes.CO2e., Target.year) %>%
-  rename("total_emissions" = Base.year.emissions..metric.tonnes.CO2e.,
-         "year" = Base.year, 
-         "last_updated" = Last.update) %>%
-  mutate(data_source = datasource_id)
+  pivot_longer(cols = c(Base.year.emissions..metric.tonnes.CO2e., 
+                        Target.year.absolute.emissions..metric.tonnes.CO2e.,
+                        Estimated.business.as.usual.absolute.emissions.in.target.year..metric.tonnes.CO2e.),
+               names_to = "emission_type",
+               values_to = "total_emissions",
+               names_repair = "unique") %>%
+  ungroup() %>%
+  mutate(emission_type = case_when(emission_type == "Base.year.emissions..metric.tonnes.CO2e." ~ "base",
+                                   emission_type == "Target.year.absolute.emissions..metric.tonnes.CO2e." ~ "target",
+                                   emission_type == "Estimated.business.as.usual.absolute.emissions.in.target.year..metric.tonnes.CO2e." ~ "BAU"),
+         year = case_when(emission_type == "base" ~ Base.year,
+                          TRUE ~ Target.year),
+         emissions_id = paste0(datasource_id, ":", emission_type, ":", locode),
+         datasource_id = datasource_id) %>%
+  filter(!is.na(total_emissions)) %>%
+  rename("actor_id" = locode) %>%
+  select(actor_id, emissions_id, emission_type, year, total_emissions, datasource_id)
+
+EmissionsTag <- EmissionsAgg %>%
+  mutate(tag_id = case_when(emission_type == "base" ~ "baseline_em",
+                            emission_type == "BAU" ~ "BAU",
+                            emission_type == "target" ~ "target_em")) %>%
+  select(emissions_id, tag_id)
+
+EmissionsAgg <- EmissionsAgg %>% 
+  select(-emission_type)
 
 Territory <- x %>%
   select(City.Location) %>%
   mutate(lat = gsub("POINT \\((.*) (.*)\\)", "\\2", City.Location), 
          lng = gsub("POINT \\((.*) (.*)\\)", "\\1", City.Location))
-  
+
 Population <- x %>%
   select(Population, Population.Year) %>%
   rename("population" = Population,
@@ -106,7 +151,17 @@ write.csv(ActorIdentifier, "Harmonized/ActorIdentifier.csv",
           row.names = F, fileEncoding = "UTF-8")
 write.csv(EmissionsAgg, "Harmonized/EmissionsAgg.csv", 
           row.names = F, fileEncoding = "UTF-8")
+write.csv(EmissionsTag, "Harmonized/EmissionsTag.csv", 
+          row.names = F, fileEncoding = "UTF-8")
 write.csv(Target, "Harmonized/Target.csv", 
           row.names = F, fileEncoding = "UTF-8")
-write.csv(TerritoryContext, "Harmonized/TerritoryContext.csv", 
+write.csv(TargetTag, "Harmonized/TargetTag.csv", 
+          row.names = F, fileEncoding = "UTF-8")
+write.csv(Publisher, "Harmonized/Publisher.csv", 
+          row.names = F, fileEncoding = "UTF-8")
+write.csv(DataSource, "Harmonized/DataSource.csv", 
+          row.names = F, fileEncoding = "UTF-8")
+write.csv(Population, "Harmonized/Population.csv", 
+          row.names = F, fileEncoding = "UTF-8")
+write.csv(Territory, "Harmonized/Territory.csv", 
           row.names = F, fileEncoding = "UTF-8")
